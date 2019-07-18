5215db795bf0257ac3096532923a02b0e5c62ce2
friedbergjon <friedberg_jon@hotmail.com>
2019-07-18 10:21:01 -0400

    intialized repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hi</h1>
+     <a target="_blank" href="https://giftme-hjm.herokuapp.com/">"Wish List App" using React and Express</a>
+     {/* https://stackoverflow.com/questions/42914666/react-router-external-link */}
+     <a target="_blank" href="https://amazing-rings.surge.sh">Currency Converter using React</a>
+     <a target="_blank" href="https://amazing-rings.surge.sh">Currency Converter using React</a>
+    </div>
+  );
+}
+
+export default App;
